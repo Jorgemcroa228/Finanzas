6c1a126ea98c1edbdab28abb4ecd063209e64170
Jorge Mario <Jorgemcroa228@gmail.com>
2026-03-03 19:01:09 -0500

    this is the first commit and I have benn created the fhater class

---
+++ src/Finances.java
@@ -0,0 +1,17 @@
+public class Finances extends App {
+
+    double balance;
+    String name;
+    int money;
+
+    public Finances(double balance, String name, int money) {
+        this.balance = balance;
+        this.name = name;
+        this.money = money;
+    }
+
+    @Override
+    public Double calculateFinances() {
+
+    }
+}
